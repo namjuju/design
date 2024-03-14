3ac7b3cdb59274a97be8e641586a230b901fc756
namjuju <157003336+namjuju@users.noreply.github.com>
2024-03-14 18:00:42 +0900

    .

--- coffee/js/common.js
+++ coffee/js/common.js
@@ -7,96 +7,71 @@
 
 $(document).ready(function(){
 
-    /*****************************************
-    브라우저가 스크롤이 되면 header fixed 클래스 추가
-    근데 맨위로 다시 올라가면 header에 fixed 클래스 삭제 
-    ******************************************/
-    let scrolling
-    function scroll_chk(){
-        scrolling = $(window).scrollTop()
-        if(scrolling > 0){ // 조금이라도 스크롤 함
-            $('.header').addClass('fixed')
-        }else{ // 스크롤 안한경우(맨위)
-            $('.header').removeClass('fixed')
-        }
-    }
-    scroll_chk() //문서로드되었을때 1번 실행
-    $(window).scroll(function(){
-        scroll_chk()
-    })
-
-    /*****************************************************
-    pc버전 메뉴에 마우스를 올리면 header에 menu_over 클래스 추가
-    이벤트 대상 : .header .gnb
-                 .header .gnb ul.depth1 > li
-                 .header .gnb ul.depth1 > li > a
-    1차메뉴 li에 마우스를 오버하면 해당 li에 on 클래스 추가 
-    이벤트 대상 : .header .gnb ul.depth1 > li
-                 .header .gnb ul.depth1 > li > a
-    이전에 마우스를 오버했던 li에는 on클래스삭제
-    ---> 이전에 오버했던 대상을 찾는 것보다.. 모든 li에 on을 삭제하고 
-         마우스를 오버한 li에만 클래스 추가
-    *********************************************************/
-    let device_status
-    let window_w
-    function device_chk(){
-        window_w = $(window).width()
-        if(window_w > 1024){ //pc버전
-            device_status = 'pc'
-        }else{ //모바일
-            device_status = 'mobile'
-        }
-        console.log(device_status)
-    }
-    device_chk() //문서가 로딩되고 1번 실행
-    $(window).resize(function(){
-        device_chk() //문서가 리사이즈될때마다 1번씩 실행
-    })
-
-    $('.header .gnb ul.depth1 > li').on('mouseenter focusin', function(){
-        if(device_status == 'pc'){
-            $('.header').addClass('menu_over')
-            $('.header .gnb ul.depth1 > li').removeClass('on')
-            $(this).addClass('on')
-        }
-    })
-    $('.header').on('mouseleave', function(){
-        if(device_status == 'pc'){
-            $('.header').removeClass('menu_over')
-            $('.header .gnb ul.depth1 > li').removeClass('on')
-        }
-    })
-    $('.header .tnb .lang').on('focusin', function(){
-        if(device_status == 'pc'){
-            $('.header').removeClass('menu_over')
-            $('.header .gnb ul.depth1 > li').removeClass('on')
-        }
-    })
-
-    /*
-    모바일 메뉴
-    */
-    $(".header .gnb ul.depth1 > li > a").on("click", function(e){
-        if(device_status == 'mobile'){
-            e.preventDefault();		/* a 태그의 href를 작동 시키지 않음 */
-            $('.header .gnb ul.depth1 > li').removeClass('on') // 2차메뉴를 다 열 수 없고 하나만 열수있다
-            $(this).parent().toggleClass('on')
-        }
-	});
-
-    $('.header .gnb .gnb_open').on('click', function(){
-        $('.header').addClass('menu_open')
-        $("html, body").css({overflow : "hidden", height : $(window).height()}).bind("scroll touchmove mousewheel", function(e){e.preventDefault();e.stopPropagation();return false;},function(){passive:false});
-    })
-    $('.header .gnb .gnb_close').on('click', function(){
-        $('.header').removeClass('menu_open')
-        $("html, body").css({overflow : "visible", height : "auto"}).unbind('scroll touchmove mousewheel');
-    })
-
-    $('.footer .family_site .open').on('click', function(){
-        $('.footer .family_site').addClass('on')
-    })
-    $('.footer .family_site .close').on('click', function(){
-        $('.footer .family_site').removeClass('on')
-    })
-})//$(document).ready
+    /******************************************** 
+       아래로 스크롤하면 header가 숨겨짐 (transform으로)
+       위로 스크롤하면 header가 나타남 (transform으로 움직임)
+       이전스크롤값(10) - 현재스크롤값(100) : 아래로 스크롤 중
+       이전스크롤값(100) - 현재스크롤값(10) : 위로 스크롤 중
+       무조건 스크롤을 내리면 header에 fixed 클래스가 들어가야함
+     **********************************************/
+     let scroll_dir //방향 - 0보다 크면 위로 스크롤
+     let scroll_prev //이전 스크롤값
+     let scroll_curr //현재 스크롤값
+ 
+     function scroll_chk(){
+         scroll_prev = scroll_curr
+         scroll_curr = $(window).scrollTop()
+         scroll_dir = scroll_prev - scroll_curr
+         console.log(scroll_dir)
+         if(scroll_curr > 250){
+             $('header').addClass('fixed')
+             if(scroll_dir > 0){ //위로스크롤 - 나타나야함
+                 $('header').attr('style','transform: translate(0, 0)')
+                 /*transform: translate(0, -100px); */
+             }else{ //아래로스크롤 - 사라져야함.
+                 $('header').attr('style','transform: translate(0, -100px)')
+             }
+         }else{
+             $('header').removeClass('fixed')
+             $('header').attr('style','transform: translate(0, 0)')
+         }
+     }
+     scroll_chk()  //처음 로딩됐을때 1번 실행
+     $(window).scroll(function(){ //스크롤 할때마다 1번 실행
+         scroll_chk() 
+     })
+ 
+     /******************************************************
+          메뉴에 마우스를 오버하면 header에 menu_over 클래스 추가
+     ********************************************************/
+     let device_status
+     let window_w
+     function device_chk(){
+         window_w = $(window).width()
+         if(window_w > 1024){ //pc버전
+             device_status = 'pc'
+         }else{ //모바일
+             device_status = 'mobile'
+         }
+         console.log(device_status)
+     }
+     device_chk() //문서가 로딩되고 1번 실행
+     $(window).resize(function(){
+         device_chk() //문서가 리사이즈될때마다 1번씩 실행
+     })
+ 
+     $('header .gnb .depth1 > li').on('mouseenter', function(){
+         if(device_status == 'pc'){
+             $('header .gnb .depth1 > li').removeClass('on')
+             $(this).addClass('on')
+             $('header').addClass('menu_over')
+         }
+     })
+     $('header').on('mouseleave', function(){
+         if(device_status == 'pc'){
+             $('header .gnb .depth1 > li').removeClass('on')
+             $('header').removeClass('menu_over')
+         }
+     })
+ 
+ })//$(document).ready
