8bee77a50fcb1c630d59c2ddc57191736810919f
namjuju <157003336+namjuju@users.noreply.github.com>
2024-04-08 17:47:29 +0900

    .

--- cjkids/js/main.js
+++ cjkids/js/main.js
@@ -42,20 +42,29 @@ $(document).ready(function(){
         disableOnInteraction: true,
     },
     loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    
+    pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+		el: '.visual .paging', /* 해당 요소의 class명 */
+		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+		renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+		    return '<span class="' + className + '">' + (index + 1) + "</span>";
+		},
+	},
     navigation: {  /* 이전, 다음 버튼 */
         nextEl: '.visual .next',  /* 다음 버튼의 클래스명 */
         prevEl: '.visual .prev',  
     },
 });
 
-$('.visual .btn_ctrl button.stop').on('click', function(){
+$('.visual .ctrl_btn button.stop').on('click', function(){
     $(this).hide()
-    $('.visual .btn_ctrl button.play').show()
+    $('.visual .visual_btn .ctrl_btn button.play').show()
     visual_swiper.autoplay.stop();  /* 일시정지 기능 */
 })
-$('.visual .btn_ctrl button.play').on('click', function(){
+$('.visual .ctrl_btn button.play').on('click', function(){
     $(this).hide()
-    $('.visual .btn_ctrl button.stop').show()
+    $('.visual .visual_btn .ctrl_btn button.stop').show()
     visual_swiper.autoplay.start();  /* 재생 기능 */
 })
 })
